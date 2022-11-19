7ce0831bcf252b5c3b4c2246baba7597d01a0299
Duc Nguyen <duc.duke.ngn@gmail.com>
2022-11-19 12:00:16 -0500

    Experiment with FE display Vue

--- frontend/src/views/main/Dashboard.vue
+++ frontend/src/views/main/Dashboard.vue
@@ -2,10 +2,18 @@
   <v-container fluid>
     <v-card class="ma-3 pa-3">
       <v-card-title primary-title>
-        <div class="headline primary--text">Dashboard</div>
+        <div class="headline primary--text">My Books</div>
       </v-card-title>
       <v-card-text>
         <div class="headline font-weight-light ma-5">Welcome {{greetedUser}}</div>
+      </v-card-text>
+    </v-card>
+    <v-card v-for="(shelf, shelfName) in getShelvesAndBooks" class="ma-3 pa-3">
+      <v-card-title primary-title>
+        <div class="headline .text-h4">{{shelfName}}</div>
+      </v-card-title>
+      <v-card-text>
+      {{shelf}}
       </v-card-text>
     </v-card>
   </v-container>
@@ -27,6 +35,38 @@ export default class Dashboard extends Vue {
         return userProfile.email;
       }
     }
+  }
+
+  get getShelvesAndBooks() {
+    const mockData = {
+      "id": 1,
+      "owner_id": 1,
+      "Reading": [
+        "1",
+        "2",
+        "3"
+      ],
+      "To Read": [
+        "5",
+        "6",
+        "7",
+        "8"
+      ],
+      "Read": [
+        "9",
+        "10",
+        "11",
+        "12"
+      ],
+      "Favorite": [
+        "100",
+        "111",
+        "112"
+      ],
+      "Recommendation": []
+    }
+    const data = Object.fromEntries(Object.entries(mockData).filter(([key]) => !["id", "owner_id"].includes(key)))
+    return data
   }
 }
 </script>
