5b946d986acd617bc8d62ae2577f7316f4c97e12
Duc Nguyen <duc.duke.ngn@gmail.com>
2022-11-22 14:08:16 -0500

    Add recommendation endpoint and suggestion logic

--- backend/backend/crud/crud_book.py
+++ backend/backend/crud/crud_book.py
@@ -1,9 +1,26 @@
+from typing import List
+
+from sqlalchemy.orm import Session
+
 from backend.models.book import Book
 from backend.crud.base import CRUDBase
 from backend.schemas.book import BookCreate, BookUpdate
 
 
 class CRUDBook(CRUDBase[Book, BookCreate, BookUpdate]):
-    pass
+    def get_suggestions(self, db: Session, favorite_genres: List[str]) -> List[str]:
+        """
+        Get Suggestions based on favorite genres declared by user.
+        Return a list of Book IDs that match the genre
+        """
+        # Always refer to genre in lowercase when interact with database.
+        favorite_genres = set([genre.lower() for genre in favorite_genres])
+        recommended_books = (
+            db.query(Book).filter(Book.genre.in_(favorite_genres)).all()
+        )
+        
+        recommended_book_ids = [book.id for book in recommended_books]
+        return recommended_book_ids
+
 
 book = CRUDBook(Book)
