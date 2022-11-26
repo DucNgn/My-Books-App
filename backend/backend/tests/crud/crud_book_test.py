5e9fa37002ed0101e4d7bb699d0de0f7af08dabd
iAmStephenH <stephenhe501@gmail.com>
2022-11-26 00:24:22 -0500

    updated book tests

--- backend/backend/tests/crud/crud_book_test.py
+++ backend/backend/tests/crud/crud_book_test.py
@@ -7,6 +7,7 @@ from backend.tests.utils import book_util
 # Variables to use in tests
 global base_book
 global optional_book
+global to_update_session
 
 
 @pytest.fixture
@@ -25,6 +26,8 @@ def test_create(db) -> None:
     created = crud.book.create(db, obj_in=test_book)
     global base_book
     base_book = created
+    global to_update_session
+    to_update_session = db
     # Assert
     assert created.title == "test_title"
     assert created.author == "test_author"
@@ -59,22 +62,14 @@ def test_get(db) -> None:
     # Do work
     retrieved = crud.book.get(db, base_book.id)
     # Assert
-    assert retrieved.title == "test_title"
-    assert retrieved.author == "test_author"
-    assert retrieved.genre == "test_genre"
+    assert book_util.compare_books(retrieved, base_book)
 
 
 def test_get_optional_parameters(db) -> None:
     # Do work
     retrieved = crud.book.get(db, optional_book.id)
     # Assert
-    assert retrieved.title == "test_title"
-    assert retrieved.author == "test_author"
-    assert retrieved.genre == "test_genre"
-    assert retrieved.isbn == 123123
-    assert retrieved.publication_year == 2022
-    assert retrieved.num_of_pages == 17
-    assert retrieved.cover_image_url is None
+    assert book_util.compare_books(retrieved, optional_book)
 
 
 # Note: PLEASE CLEAR DB BEFORE RUNNING TEST TO RUN THIS TEST
@@ -100,14 +95,13 @@ def test_remove(db) -> None:
 
 def test_update(db) -> None:
     # Set Up
-    to_update = crud.book.get(db, base_book.id)
     to_update_with = schemas.BookCreate(
         title="changed",
         author="CHANGED",
         genre="test_genre",
     )
     # Do Work
-    crud.book.update(db, db_obj=to_update, obj_in=to_update_with)
+    crud.book.update(to_update_session, db_obj=base_book, obj_in=to_update_with)
     changed = crud.book.get(db, base_book.id)
     # Assert
     assert changed.title == "changed"
@@ -115,6 +109,8 @@ def test_update(db) -> None:
     assert changed.genre == "test_genre"
 
 
+# Note: PLEASE CLEAR DB BEFORE RUNNING TEST TO RUN THIS TEST
+# Check if we can retrieve the two we just added in db + start up initial books
 def test_retrieve_suggestions(db) -> None:
     # Set Up
     list_genres = ["fantasy", "romance", "sci-fi", "comedy"]
@@ -122,6 +118,7 @@ def test_retrieve_suggestions(db) -> None:
     # Do Work
     list_recommended_id = crud.book.get_suggestions(db, favorite_genres=[list_genres[0], list_genres[2]])
     # Assert
+    # if we did not add any other books by startup script, should only have 10 relevant books in db
     assert len(list_recommended_id) == 10
     for recommended_id in list_recommended_id:
         retrieved = crud.book.get(db, recommended_id)
